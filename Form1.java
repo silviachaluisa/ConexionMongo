a8a4734de71f5e578fe961b71bc30f00870c97ff
silviachaluisa <veronica-14-1998@hotmail.com>
2024-02-20 08:27:21 -0500

    Cambio Silvia

---
+++ Form1.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Form1 {
+    private JPanel Registro;
+    private JButton button1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+}
