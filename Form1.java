416373423c7d9346823ff5c4dc4a26cec130e2f3
silviachaluisa <veronica-14-1998@hotmail.com>
2024-02-20 09:20:01 -0500

    Cambio Silvia4

--- Form1.java
+++ Form1.java
@@ -3,6 +3,8 @@ import org.bson.Document;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 public class Form1 {
     JPanel Registro;
@@ -11,9 +13,54 @@ public class Form1 {
     private JTextField textField2;
     private JTextField textField3;
     private JButton borrarButton;
+    private JButton actualizarButton;
+    private String CampoaBorrar="";
     ConexionMongoDB mongo;
 
     public Form1() {
+        // Asignar un nombre a los campos
+        textField1.setName("Nombre");
+        textField2.setName("Materia");
+        textField3.setName("Calificacion");
+
+
+        // Capturas el campo con foco
+        textField1.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                CampoaBorrar=textField1.getName();
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+
+            }
+        });
+
+        textField2.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                CampoaBorrar=textField2.getName();
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+
+            }
+        });
+
+        textField3.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                CampoaBorrar=textField3.getName();
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+
+            }
+        });
+
         mongo = new ConexionMongoDB("mongodb://localhost:27017");
         mongo.getAcceso("Registros", "Registros1");
         ingresarButton.addActionListener(new ActionListener() {
@@ -23,6 +70,33 @@ public class Form1 {
                         .append("Materia", textField2.getText())
                         .append("Calificacion", textField3.getText());
                 mongo.Insertar_documento(documento);
+            }
+        });
+        borrarButton.addActionListener(new ActionListener() {
+            String Nombre=textField1.getText();
+            String Materia=textField2.getText();
+            String Calif=textField3.getText();
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Comprobar el campo con foco
+                if (CampoaBorrar.equals("Nombre")){
+                    if (!Nombre.isEmpty()){
+                        textField1.setText(Nombre.substring(0, Nombre.length()-1));
+                    }
+                }
+
+                if (CampoaBorrar.equals("Materia")){
+                    if (!Materia.isEmpty()){
+                        textField2.setText(Materia.substring(0, Materia.length()-1));
+                    }
+                }
+
+                if (CampoaBorrar.equals("Calificacion")){
+                    if (!Calif.isEmpty()){
+                        textField3.setText(Calif.substring(0, Calif.length()-1));
+                    }
+                }
+
             }
         });
     }
